9b798fb72a548fbee45b255e6fe7ac0af42c25ba
원기 이 <dldnjsrl0121@gmail.com>
2021-05-20 20:45:18 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -2,31 +2,21 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const onSubmit = function (e) {
-        alert('submittted');
-        console.log(username);
-        console.log(password);
-    };
+    const [exCount, newCount] = useState(0);
+
+    const counter = function (incrementBy) {
+        newCount(exCount + incrementBy);
+    }
 
     return (
         <div>
             <h1>
                 useState Practice
             </h1>
-            <form onSubmit={onSubmit}>
-                    <input placeholder="id" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <br />
-                <input placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <input type ="submit" value="submit" />
-                </form>
+            <button onClick={() => counter(3)}>Counter {exCount}</button>
             </div>
     );
 }
 
-function write() {
-
-}
 
 export default App;
